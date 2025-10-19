da84b63f88ad703f06d6e592be43a88ebaf6719a
piyush <piyushkumarsaini6@gmail.com>
2025-10-19 15:19:49 +0530

    update moles file add Presence class

--- chat/models.py
+++ chat/models.py
@@ -6,9 +6,16 @@ class ChatUser(models.Model):
 	name = models.CharField(max_length=150)
 	number = models.CharField(max_length=50, unique=True)
 	password = models.CharField(max_length=128)  # store hashed or plain depending on implementation
-
+	is_online = models.BooleanField(default=False)
+	last_seen = models.DateTimeField(null=True, blank=True)
 	def __str__(self):
 		return f"{self.name} <{self.number}>"
+
+class Presence(models.Model):
+    user = models.ForeignKey(ChatUser, on_delete=models.CASCADE)
+    is_online = models.BooleanField(default=False)
+    last_seen = models.DateTimeField(null=True, blank=True)
+    device_name = models.CharField(max_length=150, null=True, blank=True)
 
 
 class ChatMessage(models.Model):
